34ce2949d688600edec81a2d2d82dd801c573462
jesusamoros <37039972+jesusamoros@users.noreply.github.com>
2019-05-26 13:44:33 +0200

    Create my_dns.sh
    update public ip on plesk, from console script

---
+++ my_dns.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#what is my public ip
+ip=$(curl ifconfig.me)
+
+#to compare
+ip2=$(cat mydns.txt)
+
+# compare ips, if ips are direfent ->  update dns.
+if [ "$ip" = "$ip2" ]; 
+then 
+echo "same ips, no need update"
+else
+date >> dns.log
+ssh  root@plesk.host  "plesk bin dns -d domain.com -a 'myip' -ip $ip" >> dns.log
+ssh  root@plesk.host  "plesk bin dns -a domain.com -a 'myip' -ip $ip" >> dns.log
+echo $ip > mydns.txt
+fi
